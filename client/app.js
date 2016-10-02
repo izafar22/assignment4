7fa383ba7a5b4f19508070ea84e70bd09f59c812
izafar22 <izafar22@gmail.com>
2016-10-03 03:51:40 +0530

    done

--- client/app.js
+++ client/app.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
     
     function init(){
-        alert('I am running');
         for(var i=0;i<=99;i++){
     $('.container').append('<div id="col"'+ i +' class="numchart"><span style="display:inline;">'+i+'</span></div>');
         }
+        $('#submit').attr({disabled:'disabled'});
         
 }
     init();
+var count=0;
+    $('.numchart').click(function(){
+    var selected=$(this).text();
+   if(count<1){    
+    $(this).html('');
+   count++;
+   $('#inputBox').attr({contentEditable:'true'});
+   $('#inputBox').blur(function(){
+ $('#submit').attr({disabled:'true'})
+   })
+   
+   
+   }
+  
+    });
 })
+
